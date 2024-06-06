274a17ae7368b7300894ec26b7493079ce26f511
JOMCY PAPPACHEN <114501649+jomcyg@users.noreply.github.com>
2024-06-06 23:40:50 +0530

    ok

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
-# terraform {
-#   backend "gcs" {
-#     bucket  = "my-tf-bucket"
-#     prefix  = "myprefix"
- 
-#     encryption_key = "xOJ22WdqRNsVssRxxtnKSGoPka6auCyfWiob1KQfs1k="
-#   }
-# }
+terraform {
+  backend "gcs" {
+    bucket  = "jmcorg-tfstate_lz"
+    prefix  = "terraform/state/tf-sample-pipeline"
+
+  }
+}
