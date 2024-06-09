03c8388d38e757e006b5fafe294a100770c15784
JOMCY PAPPACHEN <114501649+jomcyg@users.noreply.github.com>
2024-06-09 16:21:09 +0530

    ok

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-# resource "google_storage_bucket" "auto-expire" {
-#   name          = "jmcorg-no-public-access-bucket"
-#   location      = var.location
-#   project       = var.project_id
-#   force_destroy = true
+resource "google_storage_bucket" "auto-expire" {
+  name          = "jmcorg-no-public-access-bucket"
+  location      = var.location
+  project       = var.project_id
+  force_destroy = true
 
-#   public_access_prevention = "enforced"
-# }
+  public_access_prevention = "enforced"
+}
