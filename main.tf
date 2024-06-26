fb1c4be5a218e01f1225ceb9cd21649c1c3a2ab9
JOMCY PAPPACHEN <114501649+jomcyg@users.noreply.github.com>
2024-06-26 22:56:35 +0530

    ok

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "google_cloud_run_service" "default" {
     metadata {
       annotations = {
         "autoscaling.knative.dev/maxScale"      = "1000"
-         "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.instance.connection_name
+        #"run.googleapis.com/cloudsql-instances" = google_sql_database_instance.instance.connection_name
         "run.googleapis.com/client-name"        = "terraform"
       }
     }
@@ -33,62 +33,3 @@ resource "google_cloud_run_service" "default" {
   autogenerate_revision_name = true
 }
 
-resource "google_compute_instance" "apps" {
-  count        = 8
-  name         = "apps-${count.index + 1}"
-  machine_type = "f1-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1804-lts"
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral IP
-    }
-  }
-}
-
-resource "random_id" "db_name_suffix" {
-  byte_length = 4
-}
-
-locals {
-  onprem = ["192.168.1.2", "192.168.2.3"]
-}
-
-resource "google_sql_database_instance" "postgres" {
-  name             = "postgres-instance-${random_id.db_name_suffix.hex}"
-  database_version = "POSTGRES_15"
-
-  settings {
-    tier = "db-f1-micro"
-
-    ip_configuration {
-
-      dynamic "authorized_networks" {
-        for_each = google_compute_instance.apps
-        iterator = apps
-
-        content {
-          name  = apps.value.name
-          value = apps.value.network_interface.0.access_config.0.nat_ip
-        }
-      }
-
-      dynamic "authorized_networks" {
-        for_each = local.onprem
-        iterator = onprem
-
-        content {
-          name  = "onprem-${onprem.key}"
-          value = onprem.value
-        }
-      }
-    }
-  }
-}
