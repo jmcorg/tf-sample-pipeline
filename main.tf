d64e266ae13d187120bd8587128ba79efeaf8bc9
JOMCY PAPPACHEN <114501649+jomcyg@users.noreply.github.com>
2024-06-26 13:21:44 +0530

    ok

--- main.tf
+++ main.tf
@@ -10,3 +10,41 @@ resource "google_storage_bucket" "auto-expire" {
     log_object_prefix = "sentinel/logs/tf-sample-pipeline"
   }
 }
+
+resource "google_cloud_run_service" "jmcapp-crun" {
+  name     = "jmcapp-crun-srv"
+  location = var.location
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+        startup_probe {
+          initial_delay_seconds = 0
+          timeout_seconds = 1
+          period_seconds = 3
+          failure_threshold = 1
+          tcp_socket {
+            port = 8080
+          }
+        }
+        liveness_probe {
+          http_get {
+            path = "/"
+          }
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metadata.0.annotations,
+    ]
+  }
+}
