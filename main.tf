4de602bf1f6b1c14e533e0c1cd37e3b819e157aa
JOMCY PAPPACHEN <114501649+jomcyg@users.noreply.github.com>
2024-06-27 00:58:32 +0530

    destroy

--- main.tf
+++ main.tf
@@ -1,35 +1,35 @@
-resource "google_storage_bucket" "auto-expire" {
-  name          = "jmcorg-no-public-access-bucket"
-  location      = var.location
-  project       = var.project_id
-  force_destroy = true
+# resource "google_storage_bucket" "auto-expire" {
+#   name          = "jmcorg-no-public-access-bucket"
+#   location      = var.location
+#   project       = var.project_id
+#   force_destroy = true
 
-  public_access_prevention = "enforced"
-  logging {
-    log_bucket = "jmcorg-tfstate_lz"
-    log_object_prefix = "sentinel/logs/tf-sample-pipeline"
-  }
-}
+#   public_access_prevention = "enforced"
+#   logging {
+#     log_bucket = "jmcorg-tfstate_lz"
+#     log_object_prefix = "sentinel/logs/tf-sample-pipeline"
+#   }
+# }
 
-resource "google_cloud_run_service" "default" {
-  name     = "cloudrun-srv"
-  location = var.region
+# resource "google_cloud_run_service" "default" {
+#   name     = "cloudrun-srv"
+#   location = var.region
 
-  template {
-    spec {
-      containers {
-        image = "us-docker.pkg.dev/cloudrun/container/hello"
-      }
-    }
+#   template {
+#     spec {
+#       containers {
+#         image = "us-docker.pkg.dev/cloudrun/container/hello"
+#       }
+#     }
 
-    metadata {
-      annotations = {
-        "autoscaling.knative.dev/maxScale"      = "1000"
-        #"run.googleapis.com/cloudsql-instances" = google_sql_database_instance.instance.connection_name
-        "run.googleapis.com/client-name"        = "terraform"
-      }
-    }
-  }
-  autogenerate_revision_name = true
-}
+#     metadata {
+#       annotations = {
+#         "autoscaling.knative.dev/maxScale"      = "1000"
+#         #"run.googleapis.com/cloudsql-instances" = google_sql_database_instance.instance.connection_name
+#         "run.googleapis.com/client-name"        = "terraform"
+#       }
+#     }
+#   }
+#   autogenerate_revision_name = true
+# }
 
