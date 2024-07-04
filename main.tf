0b5652737d16c3092c828cb167cc4108047535c9
JOMCY PAPPACHEN <114501649+jomcyg@users.noreply.github.com>
2024-07-04 10:27:09 +0530

    ok

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-# resource "google_storage_bucket" "auto-expire" {
-#   name          = "jmcorg-no-public-access-bucket"
-#   location      = var.location
-#   project       = var.project_id
-#   force_destroy = true
+resource "google_storage_bucket" "auto-expire" {
+  name          = "jmcorg-no-public-access-bucket"
+  location      = var.location
+  project       = var.project_id
+  force_destroy = true
 
-#   public_access_prevention = "enforced"
-#   logging {
-#     log_bucket = "jmcorg-tfstate_lz"
-#     log_object_prefix = "sentinel/logs/tf-sample-pipeline"
-#   }
-# }
+  public_access_prevention = "enforced"
+  logging {
+    log_bucket = "jmcorg-tfstate_lz"
+    log_object_prefix = "sentinel/logs/tf-sample-pipeline"
+  }
+}
 
 # resource "google_cloud_run_service" "default" {
 #   name     = "cloudrun-srv"
