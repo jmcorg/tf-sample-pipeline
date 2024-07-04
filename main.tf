7f30ea63605ed441d255c36f2f38e9b64bb3ad5e
JOMCY PAPPACHEN <114501649+jomcyg@users.noreply.github.com>
2024-07-04 12:15:41 +0530

    ok

--- main.tf
+++ main.tf
@@ -11,50 +11,50 @@ resource "google_storage_bucket" "auto-expire" {
   }
 }
 
-# locals {
-#   cluster_type = "simple-regional"
-# }
-
-# data "google_client_config" "default" {}
-
-# provider "kubernetes" {
-#   host                   = "https://${module.gke.endpoint}"
-#   token                  = data.google_client_config.default.access_token
-#   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
-# }
-
-# module "gke" {
-#   source  = "terraform-google-modules/kubernetes-engine/google"
-#   version = "~> 31.0"
-
-#   project_id                  = var.project_id
-#   name                        = "${local.cluster_type}-cluster${var.cluster_name_suffix}"
-#   regional                    = true
-#   region                      = var.region
-#   network                     = var.network
-#   subnetwork                  = var.subnetwork
-#   ip_range_pods               = var.ip_range_pods
-#   ip_range_services           = var.ip_range_services
-#   create_service_account      = false
-#   service_account             = var.compute_engine_service_account
-#   enable_cost_allocation      = true
-#   enable_binary_authorization = var.enable_binary_authorization
-#   gcs_fuse_csi_driver         = true
-#   fleet_project               = var.project_id
-#   deletion_protection         = false
-#   stateful_ha                 = true
-# }
-
-resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
-  name          = "subnet-asia-south-1-jmc-gke"
-  ip_cidr_range = "10.11.0.0/16"
-  region        = "asia-south1"
-  network       = "https://www.googleapis.com/compute/v1/projects/jmc-devsecops/global/networks/jm-devsecops-vpc"
-  secondary_ip_range {
-    range_name    = "tf-gke-dev-pods-secondary-range-1"
-    ip_cidr_range = "192.168.10.0/24"
-  }
+locals {
+  cluster_type = "simple-regional"
 }
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+module "gke" {
+  source  = "terraform-google-modules/kubernetes-engine/google"
+  version = "~> 31.0"
+
+  project_id                  = var.project_id
+  name                        = "${local.cluster_type}-cluster${var.cluster_name_suffix}"
+  regional                    = true
+  region                      = var.region
+  network                     = var.network
+  subnetwork                  = var.subnetwork
+  ip_range_pods               = var.ip_range_pods
+  ip_range_services           = var.ip_range_services
+  create_service_account      = false
+  service_account             = var.compute_engine_service_account
+  enable_cost_allocation      = true
+  enable_binary_authorization = var.enable_binary_authorization
+  gcs_fuse_csi_driver         = true
+  fleet_project               = var.project_id
+  deletion_protection         = false
+  stateful_ha                 = true
+}
+
+# resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+#   name          = "subnet-asia-south-1-jmc-gke"
+#   ip_cidr_range = "10.11.0.0/16"
+#   region        = "asia-south1"
+#   network       = "https://www.googleapis.com/compute/v1/projects/jmc-devsecops/global/networks/jm-devsecops-vpc"
+#   secondary_ip_range {
+#     range_name    = "tf-gke-dev-pods-secondary-range-1"
+#     ip_cidr_range = "192.168.10.0/24"
+#   }
+# }
 
 
 
