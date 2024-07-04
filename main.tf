bbaa343d062ffe445c384d95fe99136aea01d461
JOMCY PAPPACHEN <114501649+jomcyg@users.noreply.github.com>
2024-07-04 23:40:55 +0530

    ok

--- main.tf
+++ main.tf
@@ -1,49 +1,49 @@
-resource "google_storage_bucket" "auto-expire" {
-  name          = "jmctest-no-public-access-bucket"
-  location      = var.location
-  project       = var.project_id
-  force_destroy = true
+# resource "google_storage_bucket" "auto-expire" {
+#   name          = "jmctest-no-public-access-bucket"
+#   location      = var.location
+#   project       = var.project_id
+#   force_destroy = true
 
-  public_access_prevention = "enforced"
-  logging {
-    log_bucket = "jmcorg-tfstate_lz"
-    log_object_prefix = "sentinel/logs/tf-sample-pipeline"
-  }
-}
+#   public_access_prevention = "enforced"
+#   logging {
+#     log_bucket = "jmcorg-tfstate_lz"
+#     log_object_prefix = "sentinel/logs/tf-sample-pipeline"
+#   }
+# }
 
-locals {
-  cluster_type = "simple-regional"
-}
+# locals {
+#   cluster_type = "simple-regional"
+# }
 
-data "google_client_config" "default" {}
+# data "google_client_config" "default" {}
 
-provider "kubernetes" {
-  host                   = "https://${module.gke.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
-}
+# provider "kubernetes" {
+#   host                   = "https://${module.gke.endpoint}"
+#   token                  = data.google_client_config.default.access_token
+#   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+# }
 
-module "gke" {
-  source  = "terraform-google-modules/kubernetes-engine/google"
-  version = "~> 31.0"
+# module "gke" {
+#   source  = "terraform-google-modules/kubernetes-engine/google"
+#   version = "~> 31.0"
 
-  project_id                  = var.project_id
-  name                        = "${local.cluster_type}-cluster${var.cluster_name_suffix}"
-  regional                    = true
-  region                      = var.region
-  network                     = var.network
-  subnetwork                  = var.subnetwork
-  ip_range_pods               = var.ip_range_pods
-  ip_range_services           = var.ip_range_services
-  create_service_account      = false
-  service_account             = var.compute_engine_service_account
-  enable_cost_allocation      = true
-  enable_binary_authorization = var.enable_binary_authorization
-  gcs_fuse_csi_driver         = true
-  fleet_project               = var.project_id
-  deletion_protection         = false
-  stateful_ha                 = true
-}
+#   project_id                  = var.project_id
+#   name                        = "${local.cluster_type}-cluster${var.cluster_name_suffix}"
+#   regional                    = true
+#   region                      = var.region
+#   network                     = var.network
+#   subnetwork                  = var.subnetwork
+#   ip_range_pods               = var.ip_range_pods
+#   ip_range_services           = var.ip_range_services
+#   create_service_account      = false
+#   service_account             = var.compute_engine_service_account
+#   enable_cost_allocation      = true
+#   enable_binary_authorization = var.enable_binary_authorization
+#   gcs_fuse_csi_driver         = true
+#   fleet_project               = var.project_id
+#   deletion_protection         = false
+#   stateful_ha                 = true
+# }
 
 
 
