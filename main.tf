f4366fb612f32a55c61d78641bb6d1ae476440a1
JOMCY PAPPACHEN <114501649+jomcyg@users.noreply.github.com>
2024-07-10 21:01:49 +0530

    ok

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ resource "google_storage_bucket" "auto-expire" {
   force_destroy = true
   labels                   = merge(var.labels, {  env = "nonprod", app-name = "jmcapp"  })
  
-  public_access_prevention = "enforced"
-  logging {
-    log_bucket = "jmcorg-tfstate_lz"
-    log_object_prefix = "sentinel/logs/tf-sample-pipeline"
-  }
+#   public_access_prevention = "enforced"
+#   logging {
+#     log_bucket = "jmcorg-tfstate_lz"
+#     log_object_prefix = "sentinel/logs/tf-sample-pipeline"
+#   }
 }
 
 # locals {
