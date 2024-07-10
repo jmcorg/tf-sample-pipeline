fae89a12a92d35e4da6df5d24ef2c9dc08a2c858
JOMCY PAPPACHEN <114501649+jomcyg@users.noreply.github.com>
2024-07-10 19:30:37 +0530

    ok

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-# resource "google_storage_bucket" "auto-expire" {
-#   name          = "jmctest-no-public-access-bucket"
-#   location      = var.location
-#   project       = var.project_id
-#   force_destroy = true
+resource "google_storage_bucket" "auto-expire" {
+  name          = "jmctest-no-public-access-bucket"
+  location      = var.location
+  project       = var.project_id
+  force_destroy = true
 
 #   public_access_prevention = "enforced"
 #   logging {
 #     log_bucket = "jmcorg-tfstate_lz"
 #     log_object_prefix = "sentinel/logs/tf-sample-pipeline"
 #   }
-# }
+}
 
 # locals {
 #   cluster_type = "simple-regional"
